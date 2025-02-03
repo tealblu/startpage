9cb469076ad33c3ef5c046aad7584a570ab38ff2
indigo hartsell <ckhartsell@gmail.com>
2025-02-03 11:44:28 -0500

    removed old wallpaper code

--- src/js/theme.js
+++ src/js/theme.js
@@ -1,20 +1,3 @@
-
-document.addEventListener("DOMContentLoaded", () => {
-  setTimeout(() => {  // Ensures CSS is loaded
-    let rootStyle = getComputedStyle(document.documentElement);
-    let wallpaper = rootStyle.getPropertyValue("--wallpaper").trim();
-
-    if (wallpaper) {
-      // Extract the actual URL from `url("...")`
-      wallpaper = wallpaper.replace(/^url\(["']?|["']?\)$/g, "");
-      console.log("Extracted Wallpaper URL:", wallpaper);  // Debugging
-      document.querySelector(".wallpaper").src = wallpaper;
-    }
-  }, 100);
-});
-
-
-
 // Image is opacity 0 and text is translated off screen by default
 // Add the loaded class to the image and text to animate them in
 window.onload = function () {
